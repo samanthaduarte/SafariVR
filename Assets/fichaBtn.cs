c6f51718c80dbd9570a20b943b784af82e5c4298
Ivette <car16020@uvg.edu.gt>
2018-05-30 16:42:06 -0600

    Uf

---
+++ Assets/fichaBtn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fichaBtn : MonoBehaviour {
+    public Button fichaButton;
+    public GameObject plane;
+
+    // Use this for initialization
+    void Start()
+    {
+        Button eb = fichaButton.GetComponent<Button>();
+        eb.onClick.AddListener(sOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void sOnClick()
+    {
+        plane.SetActive(true);
+    }
+}
