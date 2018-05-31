a1e9668523cbd150969abdb463a9f10e5fc1f0c1
Ivette <car16020@uvg.edu.gt>
2018-05-31 08:49:48 -0600

    chanchuye

--- Assets/triggerAnimal.cs
+++ Assets/triggerAnimal.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class triggerAnimal : MonoBehaviour {
 	private Animator anim;
@@ -9,6 +10,8 @@ public class triggerAnimal : MonoBehaviour {
 	public float Volume=1.0f;
 	AudioSource audio;
 	public bool alreadyPlayed = false;
+	//private Image ficha; 
+	//public Canvas ficha; 
 
 	//private GameObject jeep;
 
@@ -18,7 +21,10 @@ public class triggerAnimal : MonoBehaviour {
 		anim.SetBool("makeSound",false);
 
 		audio = GetComponent<AudioSource>();
-
+		//GameObject go = GameObject.Find("Canvas");
+		//ficha = go.GetComponent<Image>();
+		//ficha = GetComponent<Canvas>();
+		//ficha.gameObject.SetActive(false);
 	}
 
 	void OnTriggerEnter(Collider other){
@@ -28,6 +34,7 @@ public class triggerAnimal : MonoBehaviour {
 			if (!alreadyPlayed){
 				audio.PlayOneShot(sonidoAnimal,Volume);
 				alreadyPlayed = true;
+				//ficha.gameObject.SetActive(true); 
 			}
 			// Mostrar ficha
 			// Audio de la ficha
@@ -37,6 +44,7 @@ public class triggerAnimal : MonoBehaviour {
 	void OnTriggerExit(Collider other){
 		if (other.tag == "jeep"){
 			anim.SetBool("makeSound",false);
+			//ficha.gameObject.SetActive(false); 
 		}
 	}
 	
