52fa5286d8b293873080908192cb852359d3ea37
samanthaduarte <samanthaduarte>
2018-05-31 00:42:31 -0600

    sonidos de fichas y animales y elefante baby

---
+++ Assets/triggerAnimal.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerAnimal : MonoBehaviour {
+	private Animator anim;
+
+	public AudioClip sonidoAnimal;
+	public float Volume=1.0f;
+	AudioSource audio;
+	public bool alreadyPlayed = false;
+
+	//private GameObject jeep;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent <Animator>();
+		anim.SetBool("makeSound",false);
+
+		audio = GetComponent<AudioSource>();
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		//if (other.gameObject.tag == "jeep"){
+			anim.SetBool("makeSound",true);
+			// Emitir sonido
+			if (!alreadyPlayed){
+				audio.PlayOneShot(sonidoAnimal,Volume);
+				alreadyPlayed = true;
+			}
+			// Mostrar ficha
+			// Audio de la ficha
+		//}
+		
+	}
+	void OnTriggerExit(Collider other){
+		if (other.tag == "jeep"){
+			anim.SetBool("makeSound",false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/triggerFicha.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerFicha : MonoBehaviour {
+	public AudioClip sonidoFicha;
+	public float Volume = 1.0f;
+	AudioSource audio;
+	public bool alreadyPlayed = false;
+
+	//private GameObject jeep;
+
+	// Use this for initialization
+	void Start () {
+
+		audio = GetComponent<AudioSource>();
+
+	}
+
+	void OnTriggerEnter(Collider other){
+			// Audio de la ficha
+			if (!alreadyPlayed){
+				audio.PlayOneShot(sonidoFicha,Volume);
+				alreadyPlayed = true;
+			}
+			// Mostrar ficha
+		
+	}
+	void OnTriggerExit(Collider other){
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
